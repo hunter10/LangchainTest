70862e1cf7ba408ac610137abf6d19c55b57b483
hunter10 <hunter10apple@gmail.com>
2024-07-26 11:40:14 +0900

    Ollama 모델 적용

--- pages/02_PrivateGPT.py
+++ pages/02_PrivateGPT.py
@@ -1,11 +1,11 @@
 from langchain.prompts import ChatPromptTemplate
 from langchain.document_loaders import UnstructuredFileLoader
-from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
+from langchain.embeddings import CacheBackedEmbeddings, OllamaEmbeddings
 from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
 from langchain.storage import LocalFileStore
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS 
-from langchain.chat_models import ChatOpenAI
+from langchain.chat_models import ChatOllama
 from langchain.callbacks.base import BaseCallbackHandler
 import streamlit as st
 
@@ -29,8 +29,8 @@ class ChatCallbackHandler(BaseCallbackHandler):
         self.message_box.markdown(self.message)
         
 
-llm = ChatOpenAI(
-
+llm = ChatOllama(
+    model="mistral:latest",
     temperature=0.1, 
     streaming=True,
     callbacks=[
@@ -56,7 +56,9 @@ def embed_file(file):
 
     loader = UnstructuredFileLoader(file_path)
     docs = loader.load_and_split(text_splitter=splitter)
-    embeddings = OpenAIEmbeddings()
+    embeddings = OllamaEmbeddings(
+        model="mistral:latest"
+    )
     cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
 
     # 이부분에서 비용 발생 - 조심해야 함 (실행할때마다)
@@ -80,19 +82,29 @@ def patin_history():
 def format_docs(docs):
     return "\n\n".join(document.page_content for document in docs)
 
-prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system", 
-            """ 
-            Answer the question using ONLY the following context. If you don't know the answer, 
-            just say you don't know. DON't make anything up.
+# prompt = ChatPromptTemplate.from_messages(
+#     [
+#         (
+#             "system", 
+#             """ 
+#             Answer the question using ONLY the following context. If you don't know the answer, 
+#             just say you don't know. DON't make anything up.
 
-            Context: {context}
-            """,
-        ),
-        ("human", "{question}"),
-    ]
+#             Context: {context}
+#             """,
+#         ),
+#         ("human", "{question}"),
+#     ]
+# )
+
+prompt = ChatPromptTemplate.from_template(
+    """ 
+    Answer the question using ONLY the following context and not your training data. If you don't know the answer, 
+    just say you don't know. DON't make anything up.
+
+    Context: {context}
+    Question:{question}
+    """   
 )
 
 st.title("Private GPT")
