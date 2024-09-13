c973ebd7044aecfcc0e03bcffecd216ebebfde8c
hunter10 <hunter10apple@gmail.com>
2024-09-13 09:36:11 +0900

    API KEY 방식의 인증법 테스트

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel, Field
 
 app = FastAPI(
     title="Nicolacus Maximus Quote Giver",
     description="Get a real quote said by Nicolacus Maximus himself.",
     servers=[
-        {"url":"https://shares-guy-flows-ashley.trycloudflare.com"}
+        {"url":"https://principle-fluid-compliant-items.trycloudflare.com"}
     ]
 )
 
@@ -19,12 +19,13 @@ class Quote(BaseModel):
     description="Upon receiving a GET request this endpoint will return a real quiote said by Nicolacus Maximus himself.",
     response_description="A Quote object that contains the quote said by Nicolacus Maximus and the date when the quote was said.",
     response_model=Quote,
-    openapi_extra={
-        "x-openai-isConsequential":True,
-    },
+    # openapi_extra={
+    #     "x-openai-isConsequential":True,
+    # },
 )
 
-def get_quote():
+def get_quote(request: Request):
+    print(request.headers)
     return {
         "quote": "Life is short so eat it all.",
         "year": 1950,
